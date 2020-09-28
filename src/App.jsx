d9f29c2d7cce220226c178f6914eea3c06aba3b3
Arthur <arthurd800@wp.pl>
2020-09-28 11:37:02 +0200

    added assets directory (pictures), added starter styles, created Banner and Header components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
-import './App.css';
+import React, {useEffect} from 'react';
+import Banner from './components/Banner';
+import Header from './components/Header';
+import './styles/App.scss';
 
 function App() {
-    return <div className='App'>Awwwards1</div>;
+    useEffect(() => {
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }, []);
+    return (
+        <div className='App'>
+            <Header />
+            <Banner />
+        </div>
+    );
 }
 
 export default App;
