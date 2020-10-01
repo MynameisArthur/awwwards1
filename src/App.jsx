6045b590143a8b22118ba5cce03bbf4729ecf217
Arthur <arthurd800@wp.pl>
2020-10-01 14:18:06 +0200

    created intro animation with gsap

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,9 @@ function App() {
     useEffect(() => {
         let vh = window.innerHeight * 0.01;
         document.documentElement.style.setProperty('--vh', `${vh}px`);
+        //prevents body flashing
+        gsap.to('body', 0, {css: {visibility: 'visible'}});
+
         // timeline
         const tl = gsap.timeline();
         tl.from('.line span', 1.8, {
@@ -20,7 +23,25 @@ function App() {
             stagger: {
                 amount: 0.3,
             },
-        });
+        })
+            .to('.overlay-top', 1.6, {
+                height: 0,
+                ease: 'expo.inOut',
+                stagger: 0.4,
+            })
+            .to('.overlay-bottom', 1.6, {
+                width: 0,
+                ease: 'expo.inOut',
+                delay: -0.8,
+                stagger: 0.4,
+            })
+            .to('.intro-overlay', 0, {css: {display: 'none'}})
+            .from('.case-image img', 1.6, {
+                scale: 1.4,
+                ease: 'expo.inOut',
+                delay: -2,
+                stagger: 0.4,
+            });
     }, []);
     return (
         <div className='App'>
