29fd81f39c46b013637b9ad84346a730b24e3371
Arthur <arthurd800@wp.pl>
2020-10-01 16:13:32 +0200

    moved part of the App component to new component Home, created new directory pages with About, Approach, Services, CaseStudies components;created new component Navigation and added styles; added React router to the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import React, {useEffect} from 'react';
 import gsap from 'gsap';
-import Banner from './components/Banner';
-import Cases from './components/Cases';
-import Header from './components/Header';
-import IntroOverlay from './components/IntroOverlay';
 import './styles/App.scss';
+//component
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+//pages
+import Home from './pages/Home';
+import CaseStudies from './pages/CaseStudies';
+import Approach from './pages/Approach';
+import Services from './pages/Services';
+import About from './pages/About';
+import {Route} from 'react-router-dom';
+
+//routes
+const routes = [
+    {path: '/', name: 'Home', Component: Home},
+    {path: '/case-studies', name: 'Case Studies', Component: CaseStudies},
+    {path: '/approach', name: 'Approach', Component: Approach},
+    {path: '/services', name: 'Services', Component: Services},
+    {path: '/about-us', name: 'About Us', Component: About},
+];
 
 function App() {
     useEffect(() => {
@@ -12,44 +27,19 @@ function App() {
         document.documentElement.style.setProperty('--vh', `${vh}px`);
         //prevents body flashing
         gsap.to('body', 0, {css: {visibility: 'visible'}});
-
-        // timeline
-        const tl = gsap.timeline();
-        tl.from('.line span', 1.8, {
-            y: 100,
-            ease: 'power4.out',
-            delay: 1,
-            skewY: 7,
-            stagger: {
-                amount: 0.3,
-            },
-        })
-            .to('.overlay-top', 1.6, {
-                height: 0,
-                ease: 'expo.inOut',
-                stagger: 0.4,
-            })
-            .to('.overlay-bottom', 1.6, {
-                width: 0,
-                ease: 'expo.inOut',
-                delay: -0.8,
-                stagger: 0.4,
-            })
-            .to('.intro-overlay', 0, {css: {display: 'none'}})
-            .from('.case-image img', 1.6, {
-                scale: 1.4,
-                ease: 'expo.inOut',
-                delay: -2,
-                stagger: 0.4,
-            });
     }, []);
     return (
-        <div className='App'>
-            <IntroOverlay />
+        <>
             <Header />
-            <Banner />
-            <Cases />
-        </div>
+            <div className='App'>
+                {routes.map(({path, Component}) => (
+                    <Route key={path} exact path={path}>
+                        <Component />
+                    </Route>
+                ))}
+            </div>
+            <Navigation />
+        </>
     );
 }
 
