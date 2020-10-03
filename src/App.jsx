0ceaee574df9e23ba2b53915d2816fda0ee6b047
Arthur <arthurd800@wp.pl>
2020-10-03 11:56:19 +0200

    added handleResize function in App component which set dimensions(width,height) of the app - it's needed for changing css variable --vh

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import gsap from 'gsap';
 import './styles/App.scss';
 //component
@@ -21,16 +21,44 @@ const routes = [
     {path: '/about-us', name: 'About Us', Component: About},
 ];
 
+function debounce(fn, ms) {
+    let timer;
+    return () => {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            timer = null;
+            fn.apply(this, arguments);
+        }, ms);
+    };
+}
+
 function App() {
+    // prevents flashing
+    gsap.to('body', 0, {css: {visibility: 'visible'}});
+    const [dimensions, setDimensions] = React.useState({
+        height: window.innerHeight,
+        width: window.innerWidth,
+    });
+
     useEffect(() => {
-        let vh = window.innerHeight * 0.01;
+        let vh = dimensions.height * 0.01;
         document.documentElement.style.setProperty('--vh', `${vh}px`);
-        //prevents body flashing
-        gsap.to('body', 0, {css: {visibility: 'visible'}});
-    }, []);
+        const debouncedHandleResize = debounce(function handleResize() {
+            setDimensions({
+                height: window.innerHeight,
+                width: window.innerWidth,
+            });
+        }, 1000);
+
+        window.addEventListener('resize', debouncedHandleResize);
+        return () => {
+            window.removeEventListener('resize', debouncedHandleResize);
+        };
+    });
     return (
         <>
             <Header />
+            {console.log(dimensions)}
             <div className='App'>
                 {routes.map(({path, Component}) => (
                     <Route key={path} exact path={path}>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {NavLink, withRouter} from 'react-router-dom';
+import {ReactComponent as UpArrow} from '../assets/UpArrowCircle.svg';
 
 const Header = () => {
+    const [menuState, setMenuState] = useState({menuOpened: false});
+    useEffect(() => {
+        if (menuState.menuOpened) {
+            //run open menu animation
+        } else {
+            //run close menu animation
+        }
+    }, [menuState.menuOpened]);
     return (
         <div className='header'>
             <div className='container'>
@@ -8,9 +18,20 @@ const Header = () => {
                     <div className='logo'>
                         <a href='/'>AGENCY.</a>
                     </div>
-                    <div className='nav'>
-                        <span></span>
-                        <span></span>
+                    <div className='nav-toggle'>
+                        <div
+                            className='hamburger-menu'
+                            onClick={() => setMenuState({menuOpened: true})}
+                        >
+                            <span></span>
+                            <span></span>
+                        </div>
+                        <div
+                            className='hamburger-menu-close'
+                            onClick={() => setMenuState({menuOpened: false})}
+                        >
+                            <UpArrow />
+                        </div>
                     </div>
                 </div>
             </div>
